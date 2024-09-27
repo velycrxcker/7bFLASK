54a917291f1a41608d8d8d762783db7322f79e80
velycrxcker <vely663@gmail.com>
2024-09-27 18:01:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,81 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import pusher
+
+import mysql.connector
+import datetime
+import pytz
+
+con = mysql.connector.connect(
+    host="185.232.14.52",
+    database="u760464709_tst_sep",
+    user="u760464709_tst_sep_usr",
+    password="dJ0CIAFF="
+)
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hola, Mundo!</p>"
+    con.close()
 
+    return render_template("app.html")
+
+# Ejemplo de ruta GET usando templates para mostrar una vista
 @app.route("/alumnos")
 def alumnos():
+    con.close()
+
     return render_template("alumnos.html")
 
+# Ejemplo de ruta POST para ver cómo se envia la informacion
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
-    matricula = request.form["txtMatriculaFA"]
+    con.close()
+    matricula      = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
+
     return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
+
+# Código usado en las prácticas
+@app.route("/buscar")
+def buscar():
+    if not con.is_connected():
+        con.reconnect()
+
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM sensor_log ORDER BY Id_Log DESC")
+    registros = cursor.fetchall()
+
+    con.close()
+
+    return registros
+
+@app.route("/registrar", methods=["GET"])
+def registrar():
+    args = request.args
+
+    if not con.is_connected():
+        con.reconnect()
+
+    cursor = con.cursor()
+
+    sql = "INSERT INTO sensor_log (Temperatura, Humedad, Fecha_Hora) VALUES (%s, %s, %s)"
+    val = (args["temperatura"], args["humedad"], datetime.datetime.now(pytz.timezone("America/Matamoros")))
+    cursor.execute(sql, val)
+    
+    con.commit()
+    con.close()
+
+    pusher_client = pusher.Pusher(
+        app_id="1714541",
+        key="2df86616075904231311",
+        secret="2f91d936fd43d8e85a1a",
+        cluster="us2",
+        ssl=True
+    )
+
+    pusher_client.trigger("canalRegistrosTemperaturaHumedad", "registroTemperaturaHumedad", args)
+
+    return args
