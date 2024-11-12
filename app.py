25a45cd396f78f48878d1c59567397a395f86b21
velycrxcker <vely663@gmail.com>
2024-11-12 17:57:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, request, jsonify
 import mysql.connector
-import datetime
-import pytz
 
 app = Flask(__name__)
 
-# Conexión a la base de datos
+# Configuración de la conexión a la base de datos
 con = mysql.connector.connect(
     host="185.232.14.52",
     database="u760464709_tst_sep",
@@ -13,6 +11,7 @@ con = mysql.connector.connect(
     password="dJ0CIAFF="
 )
 
+# Ruta para obtener la lista de usuarios
 @app.route("/usuarios", methods=["GET"])
 def obtener_usuarios():
     if not con.is_connected():
@@ -20,9 +19,9 @@ def obtener_usuarios():
     cursor = con.cursor(dictionary=True)
     cursor.execute("SELECT Id_Usuario, Nombre_Usuario, Contrasena FROM tst0_usuarios")
     usuarios = cursor.fetchall()
-    con.close()
     return jsonify(usuarios)
 
+# Ruta para agregar un nuevo usuario
 @app.route("/usuarios", methods=["POST"])
 def guardar_usuario():
     if not con.is_connected():
@@ -32,10 +31,7 @@ def guardar_usuario():
     cursor = con.cursor()
     cursor.execute("INSERT INTO tst0_usuarios (Nombre_Usuario, Contrasena) VALUES (%s, %s)", (nombre, contrasena))
     con.commit()
-    con.close()
     return jsonify({"message": "Usuario guardado"})
-
-# Rutas adicionales para editar y eliminar usuarios...
 
 if __name__ == "__main__":
     app.run(debug=True)
